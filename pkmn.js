903df416123506bd2f599076a449e6edf3dc02f5
SJRNoodles <54688233+SJRNoodles@users.noreply.github.com>
2021-03-19 10:23:47 -0500

    Update pkmn.js

--- pkmn.js
+++ pkmn.js
@@ -4,8 +4,7 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(255,255,255);
   // player movements
   if(keyDown(LEFT_ARROW)){
   plr.position.x-=5;
@@ -14,9 +13,11 @@ function draw() {
   plr.position.x+=5;
   }
   if(keyDown(UP_ARROW)){
+   plr.rotation += 4;
   plr.position.y+=5;
   }
   if(keyDown(DOWN_ARROW)){
   plr.position.y-=5;
   }
+  drawSprites();
 }
